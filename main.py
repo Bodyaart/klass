d29d62fa254e58866852d51d0e995560d30d9ca0
Bodyaart <157576536+Bodyaart@users.noreply.github.com>
2024-02-14 14:07:40 +0300

    Работа у Морозова 14 02 24

---
+++ main.py
@@ -0,0 +1,36 @@
+#coding:utf-8
+#Задача 1
+N = int(input())
+if N % 3 == 0:
+    print("кратно")
+else:
+    print("не кратно")
+    
+#Задача 2 пункт 1
+x = int(input())
+y = int(input())
+print("1) z =",(min(x,y) + 0.5)/(1+max(x,y)))
+if x >=0:
+    print("2) z =",max(x,y))
+else: 
+    print("2) z =",min(x,y))
+if y >=0:
+    print("3) z =",min(x,y))
+else:
+    print("3) z =",max(x**2,y**2))
+    
+#Задача 2 пункт 2
+if x >=+0:
+    print("z) z =",max)
+else:
+    print("2) z =",min)
+if y >=0:
+    print("3) z=", min)
+else:
+    if x**2 >= y**2:
+        max = x
+    else:
+        min = y
+    print("3) z =", max)
+    
+#Задача 2 пункт 3
