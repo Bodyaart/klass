b6195e2f170d137b76cbe53832f3f71ada69202a
Bodyaart <157576536+Bodyaart@users.noreply.github.com>
2024-02-21 14:11:30 +0300

    Работа у Морозова за 21 02 24
    Да

--- main (1).py
+++ main (1).py
@@ -1,4 +1,33 @@
-#coding:utf-8
-print("Привет мир меня зовут Богдан!")#выводит приветствие на экран
-print("И тебе привет! Богдан!")#выводит ответ на экран
-
+#coding utf-8
+#Задача 1 
+c=int(input("Введите количество чисел: "))
+a=0
+b=1 
+print(a)
+print(b)
+i=0 
+while i<=c:
+    sum = (a+b)
+    print(sum)
+    i += 1 
+    a=b 
+    b=sum
+#Задача 2
+num = 0
+while num <=20:
+    print(num)
+    num +=2
+    
+num =-1
+count = 0 
+while num >= -21:
+    if count %3 == 0:
+        print(num)
+        num -=1 
+        count +=1
+        
+#Задача 3
+i = 0 
+while i < 5:
+    print("Привет мир")
+    i += 1
